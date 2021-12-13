c9b8dd2256f431f586d04236c577e3e0f0fbfc24
Salman-S-Rahman <salmansr.developer@gmail.com>
2021-12-13 23:56:12 +0600

    Array Check Method

---
+++ concat.js
@@ -0,0 +1,6 @@
+const brother1 = ["Roohullah", "Hamdullah", "Zabhiullah"];
+const brother2 = ["AsmatUllah", "Samiullah", "My Lala Jan"];
+
+const brother = brother1.concat(brother2);
+
+console.log(brother)
