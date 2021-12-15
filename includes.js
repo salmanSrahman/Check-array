50340ef567d667bedd3a04dfd0bf14870dee9220
Salman-S-Rahman <salmansr.developer@gmail.com>
2021-12-15 22:20:48 +0600

    added new work

---
+++ includes.js
@@ -0,0 +1,9 @@
+
+const friends = ["Roohullah", "Hamdullah", "Zabhiullah", "AsmatUllah", "Samiullah"]
+
+if (friends.includes("My Lala Jan")) {
+    console.log("Is Included");
+}
+else {
+    console.log("Is Not Included")
+}
