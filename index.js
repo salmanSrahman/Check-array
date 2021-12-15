516f3cc5cca0a8eed9764ce32ba93bc34b2f4d24
Salman-S-Rahman <salmansr.developer@gmail.com>
2021-12-15 22:22:31 +0600

    added new file

---
+++ index.js
@@ -0,0 +1,13 @@
+const friends = ["Roohullah", "Hamdullah", "Zabhiullah", "AsmatUllah", "Samiullah"]
+
+if (friends.indexOf("My Lala Jan") !== -1) {
+    console.log("Is exist")
+}
+else {
+    console.log("Is Not exist")
+}
+
+
+
+
+
